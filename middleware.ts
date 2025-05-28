6d4229fd1a0b1f5799799972fd28b39f0427488e
deon <ash.dawn.lacher@gmail.com>
2025-05-28 14:46:45 +0100

    mid fix

--- middleware.ts
+++ middleware.ts
@@ -140,7 +140,7 @@ import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
 const protectedRoutes = [
-  "/admin",
+  // "/admin",
   "/dashboard",
   "/profile",
   "/settings",
